73179034156b8f8593f5252c952e11abe1bb8659
Khoa Vu <khoavuexp2@gmail.com>
2026-08-06 22:22:19 +0700

    xoa key

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react'
-console.log("Kiem tra Key:", import.meta.env.VITE_GEMINI_API_KEY);
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
